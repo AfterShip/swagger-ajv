065ed8197247d7328a5b208da6a37d4c6491629d
Tushar Jain <jain00tushar@gmail.com>
2018-04-27 15:21:42 +0800

    :cactus: UPT: main file

---
+++ src/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.middlewares = {
+	express: {
+		docs: require('./middlewares/express/docs'),
+		validation: require('./middlewares/express/validation')
+	},
+	koa: {
+		docs: require('./middlewares/koa/docs'),
+		validation: require('./middlewares/koa/validation')
+	}
+};
+
+exports.utils = {
+	mergeSchemas: require('./utils/merge_schemas')
+};
