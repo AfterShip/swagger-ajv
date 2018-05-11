79d2f72cab1eef26b41ca280d09cbf90c1834d92
xiaoyufan <xiaoyu@aftership.com>
2018-05-11 12:12:12 +0800

    :ok_hand: Destructuring dir and name from path.parse

--- src/utils/merge_schemas.js
+++ src/utils/merge_schemas.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const path = require('path');
 
 const trimExt = filePath => {
-	const parsedFilePath = path.parse(filePath);
-	return `${parsedFilePath.dir}/${parsedFilePath.name}`;
+	const {dir, name} = path.parse(filePath);
+	return `${dir}/${name}`;
 };
 
 const parseDirStructure = (absoluteRootPath, absoluteFilePath) => (
