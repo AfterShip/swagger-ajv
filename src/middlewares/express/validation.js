d302410cf5539b12246e7049513dae622ee2ef1c
xiaoyufan <xiaoyu@aftership.com>
2018-05-15 13:26:20 +0800

    :bug: Use the standard express error handling way

--- src/middlewares/express/validation.js
+++ src/middlewares/express/validation.js
@@ -6,24 +6,27 @@ module.exports = ({components, paths, ajvOptions}) => {
 	const validate = validation({components, paths, ajvOptions});
 
 	return (req, _res, next) => {
-		const {
-			body,
-			headers,
-			method,
-			params,
-			route,
-			query
-		} = req;
+		try {
+			const {
+				body,
+				headers,
+				method,
+				params,
+				route,
+				query
+			} = req;
 
-		validate({
-			body,
-			headers,
-			method,
-			params,
-			query,
-			route: route.path
-		});
-
-		return next();
+			validate({
+				body,
+				headers,
+				method,
+				params,
+				query,
+				route: route.path
+			});
+			next();
+		} catch (err) {
+			next(err);
+		}
 	};
 };
